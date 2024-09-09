f1b284f9e9cff18ddfe3d62a84a470f53adaff35
David Correa <david.correav@gmail.com>
2024-09-08 21:48:40 -0500

    Test for deploy

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import "ember-widget/dist/style.css";
-import { EmberChat } from "ember-widget";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@emberai/ember-widget/dist/style.css";
+import { EmberChat } from "@emberai/ember-widget";
 
 const config = {
-    secret: import.meta.env.VITE_EMBER_API_KEY,
-  };
+  secret: import.meta.env.VITE_EMBER_API_KEY,
+};
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-        <App />
-        <EmberChat config={config} />
+      <App />
+      <EmberChat config={config} />
     </StrictMode>
   );
 } else {
-  console.error('Root element not found');
+  console.error("Root element not found");
 }
